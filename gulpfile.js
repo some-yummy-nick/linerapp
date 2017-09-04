b90d9b74c8f74a57df4bd27d7d7cb1faaf002cc4
Marat Shaymardanov <yeffasol@mail.ru>
2017-09-04 22:07:53 +0300

    Add font magician

--- gulpfile.js
+++ gulpfile.js
@@ -2,29 +2,7 @@ var gulp = require('gulp'),
   path = require('path'),
   plumber = require('gulp-plumber'),
   gutil = require('gulp-util'),
-  postcss = require('gulp-postcss'),
-  purify = require('gulp-purifycss');
-
-let processors = [
-  require('autoprefixer')(),
-  require('postcss-easysprites')({
-    imagePath: './images/sprite',
-    spritePath: './images'
-  }),
-  require('postcss-fontpath')({
-    formats: [
-      { type: 'woff2', ext: 'woff2' },
-      { type: 'woff', ext: 'woff' },
-      { type: 'truetype', ext: 'ttf' }
-    ]
-  }),
-  require('postcss-filter-gradient'),//поддержка градиентов ниже ie9
-  require("postcss-color-rgba-fallback"),//добавляет цвет если нет поддержки прозрачности
-  require('postcss-rgb-plz'),//конвертирует hex в rgb
-  require('css-mqpacker')({
-    sort: true
-  })
-];
+  postcss = require('gulp-postcss');
 
 gulp.task('css', function () {
   return gulp.src('./css/style.css')
@@ -34,8 +12,32 @@ gulp.task('css', function () {
         this.emit('end');
       }
     }))
-    .pipe(postcss(processors))
-    .pipe(gulp.dest('./css'))
+    .pipe(postcss([
+      require('postcss-assets')({
+        loadPaths: ['images/']
+      }),
+      require('postcss-font-magician')({
+        variants: {
+          'Open Sans': {
+            '400': []
+          }
+        },
+        formats: 'woff2 woff ttf'
+      }),
+      require('autoprefixer')(),
+      require('postcss-easysprites')({
+        imagePath: './images/sprite',
+        spritePath: './images'
+      }),
+      require('postcss-filter-gradient'),//поддержка градиентов ниже ie9
+      require("postcss-color-rgba-fallback"),//добавляет цвет если нет поддержки прозрачности
+      require('postcss-rgb-plz'),//конвертирует hex в rgb
+      require('css-mqpacker')({
+        sort: true
+      })
+    ])
+  )
+    .pipe(gulp.dest('./css'));
 });
 
 gulp.task("watch", function () {

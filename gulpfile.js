6f77098d81b4862bdf3e5a644d4cabb55dbaf4e6
yeffasol <yeffasol@mail.ru>
2017-09-05 17:54:25 +0300

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,53 +1,69 @@
 var gulp = require('gulp'),
-  path = require('path'),
-  plumber = require('gulp-plumber'),
-  gutil = require('gulp-util'),
-  postcss = require('gulp-postcss');
+	path = require('path'),
+	plumber = require('gulp-plumber'),
+	gutil = require('gulp-util'),
+	less = require('gulp-less'),
+	browserSync = require('browser-sync').create(),
+	postcss = require('gulp-postcss');
 
 gulp.task('css', function () {
-  return gulp.src('./css/style.css')
-    .pipe(plumber({
-      errorHandler: function (error) {
-        gutil.log('Error: ' + error.message);
-        this.emit('end');
-      }
-    }))
-    .pipe(postcss([
-      require('postcss-assets')({
-        loadPaths: ['images/']
-      }),
-      require('postcss-font-magician')({
-        variants: {
-          'Open Sans': {
-            '400': []
-          }
-        },
-        formats: 'woff2 woff ttf'
-      }),
-      require('autoprefixer')(),
-      require('postcss-easysprites')({
-        imagePath: './images/sprite',
-        spritePath: './images'
-      }),
-      require('postcss-filter-gradient'),//поддержка градиентов ниже ie9
-      require("postcss-color-rgba-fallback"),//добавляет цвет если нет поддержки прозрачности
-      require('postcss-rgb-plz'),//конвертирует hex в rgb
-      require('css-mqpacker')({
-        sort: true
-      })
-    ])
-  )
-    .pipe(gulp.dest('./css'));
+	return gulp.src('./tpl/less/style_less.less')
+
+		.pipe(plumber({
+			errorHandler: function (error) {
+				gutil.log('Error: ' + error.message);
+				this.emit('end');
+			}
+		}))
+		.pipe(less())
+		.pipe(postcss([
+				require('postcss-assets')({
+					loadPaths: ['tpl/img/']
+				}),
+				require('postcss-font-magician')({
+					variants: {
+						'Roboto Condensed': {
+							'700': []
+						}
+					},
+					formats: 'woff2 woff ttf'
+				}),
+				require('autoprefixer')(),
+				require('postcss-easysprites')({
+					imagePath: './tpl/images/sprite',
+					spritePath: './tpl/images'
+				}),
+				require('postcss-filter-gradient'),//поддержка градиентов ниже ie9
+				require("postcss-color-rgba-fallback"),//добавляет цвет если нет поддержки прозрачности
+				//require('postcss-rgb-plz'),//конвертирует hex в rgb
+				require('css-mqpacker')({
+					sort: true
+				})
+			])
+		)
+		.pipe(gulp.dest('./tpl/css'))
+		.pipe(browserSync.stream());
+});
+
+gulp.task('server', ['css'], function () {
+	browserSync.init({
+		notify: false,
+		open: false,
+		server: {
+			baseDir: './tpl'
+		}
+	});
 });
 
 gulp.task("watch", function () {
-  gulp.watch('css/style.css', ['css']);
+	gulp.watch('**/*/*.less', ['css']);
+	gulp.watch("**/*/*.html").on('change', browserSync.reload);
+	gulp.watch("js/*.js").on('change', browserSync.reload);
 });
-
 
 gulp.task("build", ['css']);
 
-gulp.task("default", ["build", "watch"]);
+gulp.task("default", ["build", "watch", "server"]);
 
 
 

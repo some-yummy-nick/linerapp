684d096dd8958f17d47a096d2fa2ddb794f05a63
Marat Shaymardanov <yeffasol@mail.ru>
2017-11-04 19:32:16 +0300

    Add modernizr

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
 const gulp = require("gulp"),
-  browserSync = require('browser-sync').create();
+  browserSync = require("browser-sync").create(),
+  jpegtran = require('imagemin-jpegtran'),
+  optipng = require('imagemin-optipng'),
+  svgo = require('imagemin-svgo'),
+  pngquant = require('imagemin-pngquant');
 
 const $ = require("gulp-load-plugins")({
   pattern: ["*"],
@@ -10,66 +14,93 @@ const onError = (err) => {
   console.log(err);
 };
 
-gulp.task('images', () =>
-  gulp.src('images/*.{jpg,png,jpeg}')
-    .pipe($.webp({
-      quality: 80,
-      preset: 'photo',
-      method: 6
-    }))
-    .pipe(gulp.dest('images'))
-);
-
-gulp.task('css', function () {
-  return gulp.src('./less/style.less')
+gulp.task("css", function () {
+  return gulp.src("./less/style.less")
     .pipe($.plumber({errorHandler: onError}))
     .pipe($.less())
     .pipe($.postcss([
-        require('postcss-assets')({
-          loadPaths: ['images/']
+        require("postcss-assets")({
+          loadPaths: ["images/"]
         }),
-        require('postcss-font-magician')({
+        require("postcss-font-magician")({
           variants: {
-            'Roboto': {
-              '700': []
+            "Roboto": {
+              "400": []
             }
           },
-          formats: 'woff2 woff ttf'
+          formats: "woff2 woff ttf"
         }),
-        require('autoprefixer')(),
-        require('postcss-easysprites')({
-          imagePath: './images/sprite',
-          spritePath: './images'
+        require("autoprefixer")(),
+        require("postcss-easysprites")({
+          imagePath: "./images/sprite",
+          spritePath: "./images"
         }),
-        require('css-mqpacker')({
+        require("css-mqpacker")({
           sort: true
         })
       ])
     )
-    .pipe(gulp.dest('./css'))
+    .pipe($.webpcss({}))
+    .pipe(gulp.dest("./css"))
     .pipe(browserSync.stream());
 });
 
-gulp.task('server', ['css'], function () {
+gulp.task("webp", () =>
+  gulp.src("images/*.{jpg,png,jpeg}")
+    .pipe($.webp({
+      quality: 80,
+      preset: "photo",
+      method: 6
+    }))
+    .pipe(gulp.dest("images"))
+);
+
+gulp.task('images', () => {
+  return gulp.src('images/*.+(jpg|JPG|png|svg)')
+    .pipe($.imagemin({
+      svgo: {
+        removeViewBox: true
+      },
+      optipng: {
+        optimizationLevel: 5
+      },
+      jpegtran: {
+        progressive: true,
+      },
+      imageminJpegRecompress: ({
+        loops: 5,
+        min: 65,
+        max: 70,
+        quality: 'medium'
+      }),
+      pngquant: [{
+        quality: '65-70', speed: 5
+      }],
+      verbose: true
+    }))
+    .pipe(gulp.dest('images'))
+});
+
+gulp.task("server", ["css"], function () {
   browserSync.init({
     notify: false,
     open: false,
     server: {
-      baseDir: './'
+      baseDir: "./"
     }
   });
 });
 
 
 gulp.task("watch", function () {
-  gulp.watch('**/*/*.less', ['css']);
-  gulp.watch("*.html").on('change', browserSync.reload);
-  gulp.watch("js/*.js").on('change', browserSync.reload);
+  gulp.watch("./less/**/*.less", ["css"]);
+  gulp.watch("*.html").on("change", browserSync.reload);
+  gulp.watch("./js/*.js").on("change", browserSync.reload);
 });
 
-gulp.task("build", ['css']);
+gulp.task("build", ["css", "images", "webp"]);
 
-gulp.task("default", ["build", "watch", "server"]);
+gulp.task("default", ["build", "watch", "server", "images", "webp"]);
 
 
 
--- js/script.js
+++ js/script.js
@@ -1,3 +1,22 @@
-(function() {
-  console.log(6);
-})();
+$(document).ready(function () {
+
+  (function () {
+    $.each($("a"), function () {
+      if ($(this).attr("href") == "" || $(this).attr("href") == "#") {
+        $(this).attr("href", "javascript:void(0)");
+      }
+    });
+  })();
+
+  (function () {
+    $("a").click(function () {
+      if ($(this).attr("href") == "" || $(this).attr("href") == "#" || $(this).attr("href") == "javascript:void(0)") {
+        return false;
+      }
+      var elementClick = $(this).attr("href");
+      $('html,body').stop().animate({scrollTop: $(elementClick).offset().top}, 1000);
+      return false;
+    });
+  })();
+
+});

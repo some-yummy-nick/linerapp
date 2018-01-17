562b4abd27cf8610e91b1f34bba4c9934290f254
Marat <yeffasol@mail.ru>
2018-01-17 17:10:56 +0300

    Add video for tablet and mobile, about, services, advantages, opportunities, clients, prices, facts blocks

--- js/script.js
+++ js/script.js
@@ -1,8 +1,22 @@
 $(document).ready(function () {
 
-  addVoidForLinks($("a"));
-  scrollLinks($(".scroll"));
-  setEqualHeight($(".row>.col"));
-  hamburger('js-hamburger', "js-menu");
+	addVoidForLinks($("a"));
+	scrollLinks($(".js-scroll"));
+	setEqualHeight($(".row>.col"));
+	hamburger('js-hamburger', "js-menu");
 
+	var mq = window.matchMedia("(max-width: 1374px)");
+	if (mq.matches) {
+
+		var mySwiper = new Swiper('.js-swiper-prices', {
+			loop:true,
+			autoHeight: true,
+			pagination: {
+				el: '.swiper-pagination',
+				clickable:true
+			}
+		})
+	}else{
+		// setEqualHeight($(".swiper-slide"));
+	}
 });
